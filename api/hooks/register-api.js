42c5ab82e63ed066510597838c3d20094b4d30d6
Panagis Tselentis <p.tselentis@ime.com.gr>
2017-03-24 11:48:38 +0200

    Deprecated register-api hook

--- api/hooks/register-api.js
+++ api/hooks/register-api.js
@@ -5,30 +5,30 @@
  * @type {ApiService|exports|module.exports}
  */
 
-var ApiService = require('../services/ApiService')
-var ip = require('ip');
+//var ApiService = require('../services/ApiService')
+//var ip = require('ip');
 
 module.exports = function hook(sails) {
   return {
 
     process: function process(next) {
 
-      var api = {
-        "name" : "kong-admin-proxy",
-        "request_path" : "/kong-proxy",
-        "strip_request_path" : true,
-        "preserve_host" : true,
-        "upstream_url" : "http://" + ip.address() + ":" + sails.config.port,
-        "plugins" : [{
-          "name" : "key-auth"
-        }]
-      }
-
-
-      ApiService.register(api,function(err,done){
-        if(err) return sails.log.warn(">> Failed to register API",err.body)
-        return sails.log.debug(">> API registered successfully!")
-      })
+      //var api = {
+      //  "name" : "kong-admin-proxy",
+      //  "request_path" : "/kong-proxy",
+      //  "strip_request_path" : true,
+      //  "preserve_host" : true,
+      //  "upstream_url" : "http://" + ( process.env.KONG_ADMIN_PROXY_HOST || ip.address() ) + ":" + sails.config.port,
+      //  "plugins" : [{
+      //    "name" : "key-auth"
+      //  }]
+      //}
+      //
+      //
+      //ApiService.register(api,function(err,done){
+      //  if(err) return sails.log.warn(">> Failed to register API",err.body)
+      //  return sails.log.debug(">> API registered successfully!")
+      //})
 
       next()
 
